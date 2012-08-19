73a49884924399afc5eff07d4f72c376444c823d
Brett Buddin <brett@brettbuddin.us>
2012-08-19 17:41:16 -0400

    Cleanup and recovery stuff

--- router.go
+++ router.go
@@ -3,21 +3,56 @@ package dapper
 import (
     "net/http"
     "regexp"
+    "log"
+    "os"
 )
 
+type Handler func(*Responder, *Request)
+
 type Router struct {
-    routes []*route
+    Routes []*route
+    Logger *log.Logger
 }
 
 func NewRouter() *Router {
-    return new(Router)
+    return &Router{
+        Logger: log.New(os.Stdout, "", log.Ldate | log.Ltime),
+    }
+}
+
+func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+    r.dispatch(w, req)
+}
+
+func (r *Router) Root(handler Handler) {
+    r.Get("", handler)
+}
+
+func (r *Router) Get(pattern string, handler Handler) {
+    r.addRoute("GET", pattern, handler)
+}
+
+func (r *Router) Post(pattern string, handler Handler) {
+    r.addRoute("POST", pattern, handler)
+}
+
+func (r *Router) Put(pattern string, handler Handler) {
+    r.addRoute("PUT", pattern, handler)
+}
+
+func (r *Router) Delete(pattern string, handler Handler) {
+    r.addRoute("DELETE", pattern, handler)
+}
+
+func (r *Router) SetLogger(logger *log.Logger) {
+    r.Logger = logger
 }
 
 func (r *Router) dispatch(w http.ResponseWriter, req *http.Request) {
     method := req.Method
     url    := req.URL
 
-    for _, route := range r.routes {
+    for _, route := range r.Routes {
         if route.method == "" || route.method != method {
             continue
         }
@@ -30,39 +65,26 @@ func (r *Router) dispatch(w http.ResponseWriter, req *http.Request) {
                 route.handler(&Responder{w}, &Request{Request: req, Params: params})
             }
 
+            defer r.rescue(w, req)
             http.HandlerFunc(handler)(w, req)
-            recover()
+
+            return
         }
     }
 }
 
-func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-    r.dispatch(w, req)
+func (r *Router) rescue(w http.ResponseWriter, req *http.Request) {
+    if err := recover(); err != nil {
+        r.Logger.Printf("Handler error: %s", err)
+        w.WriteHeader(500)
+    }
 }
 
-func (r *Router) Root(handler func(*Responder, *Request)) {
-    r.Get("", handler)
-}
-
-func (r *Router) Get(pattern string, handler func(*Responder, *Request)) {
-    r.addRoute("GET", pattern, handler)
-}
-
-func (r *Router) Post(pattern string, handler func(*Responder, *Request)) {
-    r.addRoute("POST", pattern, handler)
-}
-
-func (r *Router) Put(pattern string, handler func(*Responder, *Request)) {
-    r.addRoute("PUT", pattern, handler)
-}
-
-func (r *Router) Delete(pattern string, handler func(*Responder, *Request)) {
-    r.addRoute("DELETE", pattern, handler)
-}
-
-func (r *Router) addRoute(method, pattern string, handler func(*Responder, *Request)) {
+func (r *Router) addRoute(method, pattern string, handler Handler) {
     exp := r.matcherPattern(pattern)
-    r.routes = append(r.routes, &route{method: method, matcher: exp, handler: handler})
+    r.Routes = append(r.Routes, &route{method: method, matcher: exp, handler: handler})
+
+    r.Logger.Printf("Registered route: /%s/", exp.String())
 }
 
 func (r *Router) matcherPattern(pattern string) *regexp.Regexp {
@@ -82,7 +104,7 @@ func (r *route) processMatches(matches [][]string) map[string]string {
 
     for i, value := range names {
         if value != "" {
-            out[value] = matches[0][i] 
+            out[value] = matches[0][i]
         }
     }
 
